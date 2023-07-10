2e3996a7418351bd1842f68c4065cbdfd3bb4191
Abinash Gouda <112660697+AbinashGouda2002@users.noreply.github.com>
2023-07-10 18:50:14 +0530

    Update Db.js

--- Db/Db.js
+++ Db/Db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const dbURL = process.env.DB
+const DB = 'mongodb+srv://abinash123:Abinash1234@nodejsbasics.orpbt68.mongodb.net/todo?retryWrites=true&w=majority'
 mongoose.set('strictQuery', false)
 const db=mongoose.connect(dbURL)
   .then((result) => {
